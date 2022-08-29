4c89dccdb46fcae1b7bfb315c5686c5ed9322436
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-08-29 11:08:02 -0500

    added CardActionArea to CardMedia to make area to click on more intuitive

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
+
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardActions from '@mui/material/CardActions';
 import IconButton from '@mui/material/IconButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHandPointer, faPepperHot } from '@fortawesome/free-solid-svg-icons'
+import { faPepperHot } from '@fortawesome/free-solid-svg-icons'
 import { Box } from '@mui/system';
+import { CardActionArea } from '@mui/material';
 
-import { useState } from 'react';
+
 
 function GalleryItem ({item, updateLikeCount}) {
 
@@ -20,32 +23,36 @@ function GalleryItem ({item, updateLikeCount}) {
     return <Card elevation={3}>
         <Box onClick={togglePhoto}>
             { displayPhoto ?
-                <CardMedia 
-                    sx={{ 
-                        width: 175,
-                        height: 175,
-                        '&:hover': {
-                            opacity: 0.7},
-                        }}
-                    component="img" 
-                    image={item.path}/> :
-                <CardMedia
-                    sx={{ 
-                        width: 175,
-                        height: 175,
-                        color: "#A31919",
-                        fontSize: "1.2rem",
-                        fontFamily: "'Dosis', sans-serif",
-                        backgroundColor: "#C9E05E",
-                        '&:hover': {
-                            backgroundColor: "#CFE470",
-                            cursor: 'default'},
+                <CardActionArea>
+                    <CardMedia 
+                        sx={{ 
+                            width: 175,
+                            height: 175,
+                            '&:hover': {
+                                opacity: 0.7},
+                            }}
+                        component="img" 
+                        image={item.path}/> 
+                </CardActionArea>
+                :
+                <CardActionArea>
+                    <CardMedia
+                        sx={{ 
+                            width: 175,
+                            height: 175,
+                            color: "#A31919",
+                            fontSize: "1.2rem",
+                            fontFamily: "'Dosis', sans-serif",
+                            backgroundColor: "#C9E05E",
+                                '&:hover': {
+                            backgroundColor: "#CFE470"},
                         display: 'flex',
                         flexDirection: 'column',
                         justifyContent: 'center'
                         }}>
                     {item.description}
-                </CardMedia>
+                    </CardMedia>
+                </CardActionArea>
             }
         </Box> 
         <Box sx={{backgroundColor: "#DBEB94"}}>
