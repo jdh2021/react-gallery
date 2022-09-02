a949c59b3696e136619dbea48c963f52987efe4f
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-09-02 11:50:00 -0500

    updated AddItem.jsx with inputs, value and onChange attributes. added mui dialog with state. styled dialog and AddItem card. testedadding image.

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,54 +1,95 @@
+import { useState} from 'react';
+import { Box } from '@mui/system';
+import { CardActionArea } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faPepperHot, faXmark, faUpload } from '@fortawesome/free-solid-svg-icons';
+
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
-import CardActions from '@mui/material/CardActions';
-import Grid from '@mui/material/Grid';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 import IconButton from '@mui/material/IconButton';
-import { Box } from '@mui/system';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import TextField from '@mui/material/TextField';
 
-function AddItem ({addItem}) {
-    return <Grid item>
-        <Card elevation={3}>
-            <form onSubmit={addItem}>
-                <Box>
+
+function AddItem ({addItem, itemDescription, itemPath, setItemPath, setItemDescription}) {
+
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return <Card 
+                elevation={3}
+                sx={{ 
+                    width: 75,
+                    height: 75}}>
+            <CardActionArea>
+                <Box onClick={()=> setOpen(!open)}>
                     <CardMedia sx={{ 
-                        width: 175,
-                        height: 175,
+                        width: 75,
+                        height: 75,
                         color: "#A31919",
                         fontSize: "1.2rem",
                         fontFamily: "'Dosis', sans-serif",
-                        backgroundColor: "#C9E05E",
+                        backgroundColor: "#DBEB94",
                         display: 'flex',
                         flexDirection: 'column',
                         justifyContent: 'center'
                         }}>
-                        <span>Find a pepper?</span>
-                        <span>Add a pepper!</span>
-                    </CardMedia>
-                </Box> 
-                <Box sx={{backgroundColor: "#DBEB94"}}>
-                    <CardActions 
-                        disableSpacing color="error"
-                        sx= {{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'space-between'
-                        }}>
                         <Box>
-                            <IconButton 
-                                aria-label="add to favorites" 
-                                color="error"
-                                type="submit"
-                                onClick={() => addItem }>
-                                <FontAwesomeIcon icon={faPlus}/>
-                            </IconButton>
+                            <FontAwesomeIcon
+                                icon={faPepperHot}
+                                color="#d32f2f"
+                                size="lg"/>
+                            <FontAwesomeIcon 
+                                color="#d32f2f"
+                                icon={faPlus}
+                                size="xs"/>
                         </Box>
-                    </CardActions>
-                </Box>
-            </form>
-        </Card>
-    </Grid> 
+                        </CardMedia>
+                </Box> 
+            </CardActionArea>
+        <Dialog 
+            open={open}    
+            onClose={handleClose}
+            PaperProps={{ sx: { backgroundColor: "#DBEB94", color: "#A31919"} }}>
+            <DialogTitle>Upload a pepper picture</DialogTitle>
+            <DialogContent>
+                <TextField
+                    color="error"
+                    margin="dense"
+                    id="pepper-description"
+                    label="Pepper Type"
+                    fullWidth
+                    variant="standard"
+                    value={itemDescription}
+                    onChange={(event) => setItemDescription(event.target.value)}
+                    />
+                <TextField
+                    color="error"
+                    margin="dense"
+                    id="pepper-path"
+                    label="URL"
+                    fullWidth
+                    variant="standard"
+                    value={itemPath}
+                    onChange={(event) => setItemPath(event.target.value)}
+                    />
+            </DialogContent>
+            <DialogActions>
+                <IconButton onClick={addItem}>
+                    <FontAwesomeIcon color="#A31919" icon={faUpload} />
+                </IconButton>
+                <IconButton onClick={()=> setOpen(!open)}>
+                    <FontAwesomeIcon color="#90B001" icon={faXmark} />
+                </IconButton>
+            </DialogActions>
+        </Dialog>
+    </Card>
 }
 
 export default AddItem;
